979c0d3bd1b6e3464002b6a53282e1e5417d38af
jbg <jbg4354@naver.com>
2025-07-07 17:09:13 +0900

    axios token 추가

--- ecommerce/src/api/axios.js
+++ ecommerce/src/api/axios.js
@@ -6,4 +6,23 @@ const instance = axios.create({
   // baseURL: 'http://localhost:8000/api'
 });
 
+// 요청 인터셉터 추가
+instance.interceptors.request.use(
+  (config) => {
+    // 로컬 스토리지에서 토큰을 가져옴
+    const token = localStorage.getItem('accessToken');
+    
+    // 토큰이 존재하면, 모든 요청 헤더에 Authorization 헤더를 추가
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    
+    return config; // 수정된 설정으로 요청을 계속 진행
+  },
+  (error) => {
+    // 요청 에러 처리
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
